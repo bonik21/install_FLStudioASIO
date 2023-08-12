cba889a7f285646030e6fe41436d122f4c5e09b8
BoniK <mail@bonik.me>
2023-08-12 22:20:58 +0900

    최초 커밋

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# install_FLStudioASIO
+# FL Studio ASIO 드라이버 관리 스크립트
+가상 ASIO 드라이버인 FL Studio ASIO 드라이버를 설치, 제거하는 윈도우용 batch file입니다.
+
+# FL Studio ASIO 드라이버의 장단점
+- ASIO4all은 WDM 기반. DAW와 윈도우 사운드 동시재생 안됨.
+- FL Studio ASIO는 WASAPI 기반. DAW와 윈도우 사운드 동시재생 가능.
+- FL Studio ASIO가 ASIO4all에 비해 레이턴시가 더 긴 편임.
+
+# 사용방법
+## 스크립트 파일 다운로드
+- install_FLStudioASIO.zip파일을 PC의 적당한 곳에 다운로드 후 압축 풀기
+
+## FL Studio ASIO 드라이버 설치
+- install_FLStudioASIO.bat 파일을 마우스 우클릭 후 관리자 권한으로 실행
+
+## FL Studio ASIO 드라이버 제거
+- uninstall_FLStudioASIO.bat 파일을 마우스 우클릭 후 관리자 권한으로 실행
---
+++ install_FLStudioASIO.bat
@@ -0,0 +1,39 @@
+@ECHO off
+@chcp 65001 1> NUL 2> NUL
+
+TITLE FL Studio ASIO 드라이버 설치
+
+:SET_VAR
+SET TITEL_LINE=####################################################################
+SET LINE=--------------------------------------------------------------------
+SET SCRIPT_VERSION=1.0
+SET AUTHOR=BoniK (https://bonik.me)
+
+:GO_TO_Batch_PATH
+pushd "%~dp0"
+
+:INSTALL_FLStudioASIO
+ECHO:%TITEL_LINE%
+ECHO:		FL Studio ASIO 드라이버 설치 Ver%SCRIPT_VERSION%
+ECHO:		작성자 : %AUTHOR%
+ECHO:%TITEL_LINE%
+ECHO:%LINE%
+ECHO:    STEP 01. FL Studio ASIO 드라이버 설치 
+ECHO:%LINE%
+
+ECHO:윈도우에 FL Studio ASIO 드라이버를 설치합니다. 
+ECHO:계속하려면 엔터키를 누르세요.
+pause
+
+ECHO.
+ECHO:ILWASAPI2ASIO.DLL를 설치합니다.
+regsvr32.exe ILWASAPI2ASIO.DLL
+ECHO:ILWASAPI2ASIO_x64.DLL를 설치합니다.
+regsvr32.exe ILWASAPI2ASIO_x64.DLL
+ECHO.
+
+:END
+ECHO:FL Studio ASIO 드라이버 설치가 완료되었습니다.
+ECHO:엔터키를 눌러 스크립트를 종료합니다.
+pause
+exit
